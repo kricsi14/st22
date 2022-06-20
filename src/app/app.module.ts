859a88a2e278d7af4f88d1bcba093a33666fb2f1
Richard Keki <richard.keki@dynata.com>
2022-06-20 13:10:34 +0200

    add tailwind, init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { ToolbarComponent } from './toolbar/toolbar.component'
+import { PlannerComponent } from './planner/planner.component'
+import { PlanComponent } from './plan/plan.component'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    PlannerComponent,
+    PlanComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/plan/plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'st-plan',
+  templateUrl: './plan.component.html',
+  styleUrls: ['./plan.component.scss']
+})
+export class PlanComponent implements OnInit {
+
+  constructor () { }
+
+  ngOnInit (): void {
+  }
+
+}
